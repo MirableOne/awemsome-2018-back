53ba77eb486e523e70d14725fb4f8da02c66d4fe
Mirable <menchenyadmitry@gmail.com>
2018-10-20 17:46:56 +0300

    add user list request

--- app/Http/Controllers/User.php
+++ app/Http/Controllers/User.php
@@ -8,12 +8,21 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Support\Facades\DB;
 
+use Illuminate\Http\Request;
+
 class User extends BaseController
 {
-    public function get()
+    public function get(Request $request)
     {
-        return  response()
-            ->json(DB::table('users')->get()->toArray())
+        $_group = DB::table('users')
+            ->select(['users.user_id', 'users.nick'])
+            ->join('users_groups_ids', 'users_groups_ids.user_id', '=', 'users.user_id')
+            ->join('groups', 'groups.group_id', '=', 'users_groups_ids.group_id')
+            ->where('groups.group_id', $request->input('group_id'))
+            ->get();
+
+        return response()
+            ->json($_group)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
